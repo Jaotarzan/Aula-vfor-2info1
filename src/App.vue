87968c80b8943ce0ae7d1ee0b0d5b06fef5b9f92
Jaotarzan <joaovssouza59@gmail.com>
2024-03-07 09:06:36 -0300

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref, reactive, onMounted } from 'vue'
+
+const cidades = ref([
+    'São Paulo',
+    'Rio de Janeiro',
+    'Belo Horizonte',
+    'Salvador',
+    'Fortaleza',
+    'Curitiba',
+    'Manaus',
+    'Recife',
+    'Porto Alegre',
+    'Brasília'
+])
+
+const cidadeNova = ref('')
+
+function adicionarCidade() {
+  if (cidadeNova.value != "") {
+    cidades.value.push(cidadeNova.value)
+    cidadeNova.value = ""
+  } 
+}
+
+</script>
+
+<template>
+ <div class="cidades">
+  <h1>lista de cidades</h1>
+  <ul>
+    <li v-for="(cidade, key) in cidades.sort()" :key="key">{{ cidade }}</li>
+  </ul>
+  <label>Informe se deseja adicionar cidades:</label>
+  <div>
+  <input type="text" v-model="cidadeNova" >
+</div>  
+  <button @click="adicionarCidade">adicionar cidade</button>
+ </div>
+</template>
+
+<style scoped>
+
+</style>
