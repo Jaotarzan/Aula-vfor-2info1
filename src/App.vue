13d5bf25d5fa9eb1c514584894d05294c612d8e5
Jaotarzan <joaovssouza59@gmail.com>
2024-03-12 20:02:39 -0300

    feat: Exclusão de cidades

--- src/App.vue
+++ src/App.vue
@@ -2,48 +2,51 @@
 import { ref, computed } from 'vue'
 
 const cidades = ref([
-    'São Paulo',
-    'Rio de Janeiro',
-    'Belo Horizonte',
-    'Salvador',
-    'Fortaleza',
-    'Curitiba',
-    'Manaus',
-    'Recife',
-    'Porto Alegre',
-    'Brasília'
+  'São Paulo',
+  'Rio de Janeiro',
+  'Belo Horizonte',
+  'Salvador',
+  'Fortaleza',
+  'Curitiba',
+  'Manaus',
+  'Recife',
+  'Porto Alegre',
+  'Brasília'
 ])
 
-const CidadesOrdenadas = computed (() => {
+const CidadesOrdenadas = computed(() => {
   return cidades.value.sort()
 })
 
 const cidadeNova = ref('')
 
 function adicionarCidade() {
-  if (cidadeNova.value != "") {
+  if (cidadeNova.value != '') {
     cidades.value.push(cidadeNova.value)
-    cidadeNova.value = ""
-  } 
+    cidadeNova.value = ''
+  }
 }
 
-
+function excluirCidade(i) {
+  cidades.value.splice(cidades.value.indexOf(i), 1)
+}
 </script>
 
 <template>
- <div class="cidades">
-  <h1>lista de cidades</h1>
-  <ul>
-    <li v-for="(cidade, key) in CidadesOrdenadas" :key="key">{{ cidade }}</li>
-  </ul>
-  <label>Informe se deseja adicionar cidades:</label>
-  <div>
-  <input type="text" v-model="cidadeNova" >
-</div>  
-  <button @click="adicionarCidade">adicionar cidade</button>
- </div>
+  <div class="cidades">
+    <h1>lista de cidades</h1>
+    <ul>
+      <li v-for="(cidade, key) in CidadesOrdenadas" :key="key">
+       <p> {{ cidade }} </p>
+        <button @click="excluirCidade(cidade)">excluir</button>
+      </li>
+    </ul>
+    <label>Informe se deseja adicionar cidades:</label>
+    <div>
+      <input type="text" v-model="cidadeNova" />
+    </div>
+    <button @click="adicionarCidade">adicionar cidade</button>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
